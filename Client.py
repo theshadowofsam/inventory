c223ca000583ca7672073cd251f781b855d0efbb
Samuel Lee <theshadowofsam@gmail.com>
2022-11-11 12:55:15 -0500

    began updating of client to use json

--- Client.py
+++ Client.py
@@ -12,16 +12,28 @@ PORT = 47468
 CODE_CLOSE = 'CODE_CLOSE'
 CODE_END = 'CODE_END'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        t = input("Enter text to send: ")
-        if t == CODE_END:
-            s.send(b'CODE_END')
-            break
-        if t == CODE_CLOSE:
-            s.send(b'CODE_CLOSE')
-            break
-        else:
-            s.send(t.encode())
-    s.close()
+class JSON_DATA:
+    def __init__(self) -> None:
+        pass
+
+class SOCK_CONN:
+    def __init__(self) -> None:
+        pass
+            
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            while True:
+                t = input("Enter text to send: ")
+                if t == CODE_END:
+                    s.send(b'CODE_END')
+                    break
+                if t == CODE_CLOSE:
+                    s.send(b'CODE_CLOSE')
+                    break
+                else:
+                    s.send(t.encode())
+            s.close()
+
+if __name__ == "__main__":
+    pass
