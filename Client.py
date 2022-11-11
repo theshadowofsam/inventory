6d493498e2a0e71fd01b7ef89a141719099883c8
sam <theshadowofsam@gmail.com>
2022-11-10 22:53:25 -0500

    Day 1 tests

--- Client.py
+++ Client.py
@@ -3,3 +3,25 @@ Samuel Lee
 Client.py
 11/09/2022
 """
+import socket
+import json
+import time
+
+HOST = '127.0.0.1'
+PORT = 47468
+CODE_CLOSE = 'CODE_CLOSE'
+CODE_END = 'CODE_END'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        t = input("Enter text to send: ")
+        if t == CODE_END:
+            s.send(b'CODE_END')
+            break
+        if t == CODE_CLOSE:
+            s.send(b'CODE_CLOSE')
+            break
+        else:
+            s.send(t.encode())
+    s.close()
--- Server.py
+++ Server.py
@@ -4,3 +4,53 @@ Server.py
 11/09/2022
 """
 import socket
+import multiprocessing
+import json
+
+class SERVER:
+    def __init__(self):
+        self.HOST = '127.0.0.1'
+        self.PORT = 47468
+        self.PROCESSES = []
+        self.COUNT = 0
+        self.SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.CLOSE = False
+    
+    def main(self):
+            self.SOCKET.bind((self.HOST, self.PORT))
+            self.SOCKET.listen()
+            self.SOCKET.settimeout(2)
+            while True:
+                conn = None
+                addr = None
+                if self.CLOSE:
+                    break
+                try:
+                    conn, addr = self.SOCKET.accept()
+                except:
+                    continue
+                if conn:
+                    p = multiprocessing.Process(target=self.handle_connection, args=[conn, addr])
+                    p.run()
+                    self.COUNT += 1
+            self.SOCKET.close()
+            print(f"count = {self.COUNT}")
+            return 1
+
+    def handle_connection(self, conn, addr):
+        while True:
+            data = conn.recv(4096)
+            print(data.decode('utf-8'))
+            if data.decode('utf-8').endswith('CODE_END'):
+                print('end')
+                break
+            if data.decode('utf-8').endswith('CODE_CLOSE'):
+                self.CLOSE = True
+                print('close')
+                break
+        conn.close()
+        return 1
+
+if __name__ == "__main__":
+    s = SERVER()
+    s.main()
