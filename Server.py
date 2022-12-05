c7cd536438d9d0a9d79cdddc997e0b6f233a5c52
sam <theshadowofsam@gmail.com>
2022-12-04 22:16:33 -0500

    adding some methods

--- Server.py
+++ Server.py
@@ -23,40 +23,39 @@ class SERVER:
         self.HOST = '127.0.0.1'
         self.PORT = 47468
         self.CONTROLLER_PORT = 47469
-        self.CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.CONTROLLER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.processes = {}
-        self.request_queue = Queue(maxsize=500)
-        self.controller_queue = Queue(maxsize=500)
+        self.CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_processes = {}
+        self.controller_process = None
+        self.client_request_queue = Queue(maxsize=500)
+        self.controller_send_queue = Queue(maxsize=500)
+        self.controller_recieve_queue = Queue(maxsize=500)
 
     def run(self):
-            self.CLIENT_SOCKET.bind((self.HOST, self.PORT))
-            self.CLIENT_SOCKET.listen()
-            self.CLIENT_SOCKET.settimeout(0)
-            while True:
-                try:
-                    self.CONTROLLER_SOCKET.connect(self.HOST)
-                except:
-                    print('Attempting connection to controller...')
-                    continue
-                else:
-                    break
-            while True:
-                connection = None
-                address = None
-                try:
-                    connection, address = self.CLIENT_SOCKET.accept()
-                    self.processes[address] = (Process(target=self.handle_connection, args=(connection, address)), Queue(maxsize=10))
-                except TimeoutError as e:
-                    pass
-                try:
-                    request = self.request_queue.get_nowait()
-                except Empty as e:
-                    print(type(e), f': {e}')
+        self.prepare_client_socket()
+        self.connect_controller()
+        self.controller_process = self.spawn_controller_process(self.CONTROLLER_SOCKET)
+        self.controller_process.run()
+        
+        while True:
+            connection = None
+            address = None
+            request = None
+            try:
+                connection, address = self.CLIENT_SOCKET.accept()
+                connection.settimeout(None)
+                self.client_processes[address] = (Process(target=self.handle_connection, args=(connection, address)), Queue(maxsize=10))
+            except TimeoutError as e:
+                pass
+            try:
+                request = self.client_request_queue.get_nowait()
 
-            self.CLIENT_SOCKET.close()
-            print(f"count = {self.COUNT}")
-            return 1
+            except Empty as e:
+                print(type(e), f': {e}')
+
+        self.CLIENT_SOCKET.close()
+        print(f"count = {self.COUNT}")
+        return 1
 
     def handle_connection(self, conn, addr):
         while True:
@@ -72,16 +71,50 @@ class SERVER:
                 code = data.decode()
                 conn.send(b'GO')
                 more_data = conn.recv(1024)
-                print(json.loads(more_data.decode()))
+                print(more_data.decode())
             else:
-                print("received something weird, stopping")
-                break
+                conn.send(b'BAD_CODE')
+                print("received something weird, retrying.")
+                continue
         conn.close()
         return 1
 
+    def spawn_controller_process(self, controller_socket):
+        return Process(target=self.handle_controller, args=(controller_socket,))
+
+    def handle_controller(self, connection):
+        while True:
+            send = None
+            receive = None
+            try:
+                send = self.controller_send_queue.get_nowait()
+            except Empty:
+                pass
+            try:
+                receive = self.controller_recieve_queue.get_nowait()
+            except Empty:
+                pass
+
+    def prepare_client_socket(self):
+        self.CLIENT_SOCKET.bind((self.HOST, self.PORT))
+        self.CLIENT_SOCKET.settimeout(0)
+        self.CLIENT_SOCKET.listen()
+
+    def connect_controller(self):
+        while True:
+            try:
+                self.CONTROLLER_SOCKET.connect(self.HOST)
+            except:
+                print('Attempting connection to controller...')
+                continue
+            else:
+                print('Controller Connected...')
+                return 1
+
+
     def cleanup_client_connection(self, address):
-        if address in self.processes.keys():
-            self.processes.pop(address)
+        if address in self.client_processes.keys():
+            self.client_processes.pop(address)
         else:
             raise KeyError
 
